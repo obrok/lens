4d9ef462488fb0074f5e6cc3249cec62abfb658b
Victor Borja <vborja@apache.org>
2016-11-03 16:51:54 -0600

    Better name for access function at index

--- lib/lens.ex
+++ lib/lens.ex
@@ -20,8 +20,8 @@ defmodule Lens do
 
   deflens at(index) do
     fn data, fun ->
-      {res, updated} = fun.(data_get_at(data, index))
-      {[res], data_set_at(data, index, updated)}
+      {res, updated} = fun.(get_at_index(data, index))
+      {[res], set_at_index(data, index, updated)}
     end
   end
 
@@ -122,11 +122,11 @@ defmodule Lens do
     {Enum.concat(res), changed}
   end
 
-  defp data_get_at(data, index) when is_tuple(data), do: elem(data, index)
-  defp data_get_at(data, index), do: Enum.at(data, index)
+  defp get_at_index(data, index) when is_tuple(data), do: elem(data, index)
+  defp get_at_index(data, index), do: Enum.at(data, index)
 
-  defp data_set_at(data, index, value) when is_tuple(data), do: put_elem(data, index, value)
-  defp data_set_at(data, index, value) when is_list(data) do
+  defp set_at_index(data, index, value) when is_tuple(data), do: put_elem(data, index, value)
+  defp set_at_index(data, index, value) when is_list(data) do
     List.update_at(data, index, fn _ -> value end)
   end
 
