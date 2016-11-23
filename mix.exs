e0438c61e023e7814e93582d8a0353275f8a4c23
Paweł Obrok <pawel.obrok@gmail.com>
2016-11-23 14:23:07 +0100

    Release 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lens.Mixfile do
   def project do
     [
       app: :lens,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
